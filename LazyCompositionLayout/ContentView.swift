f46cc6040fb1bfa07fdc61132e37ee5a56d8f1b0
Tal Spektor <tal.s@zemingo.com>
2022-04-28 23:43:28 +0300

    Initial Commit

---
+++ LazyCompositionLayout/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LazyCompositionLayout
+//
+//  Created by Tal talspektor on 28/04/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LazyCompositionLayout/LazyCompositionLayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyCompositionLayoutApp.swift
+//  LazyCompositionLayout
+//
+//  Created by Tal talspektor on 28/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct LazyCompositionLayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
